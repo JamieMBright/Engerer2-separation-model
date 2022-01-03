1c45e5313634cdb47544a6dc590295cb7cc99825
Dr Jamie M. Bright <JamieMBright@users.noreply.github.com>
2022-01-03 11:00:55 +0000

    Update Engerer2Separation.m
    Corrected TJ implementation from radian sin to degrees sin function.

--- Engerer2Separation.m
+++ Engerer2Separation.m
@@ -119,9 +119,9 @@ E0h = Eextn.*cosd(zen);
 E0h(zen>90)=0;
 
 % The TJ clear-sky model
-A = 1160 + 75 .* sin((360.*(doy-275))./365);
-k = 0.174 + 0.035 .* sin((360.*(doy-100))./365);
-C = 0.095 + 0.04 .* sin((360.*(doy-100))./365);
+A = 1160 + 75 .* sind((360.*(doy-275))./365);
+k = 0.174 + 0.035 .* sind((360.*(doy-100))./365);
+C = 0.095 + 0.04 .* sind((360.*(doy-100))./365);
 Ebncs = A .* exp(-k./cosd(zen));
 Ebncs(zen>90) = 0;
 Eghcs = Ebncs.*cosd(zen) + C.*Ebncs;
